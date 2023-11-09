9eea76a8026c37de3f1ac146f2ba749f729c603b
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2023-11-08 22:38:18 -0800

    improvement: add clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,87 @@
 // src/main.rs
 
 // dependencies
+use clap::Parser;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::Error;
+use std::collections::HashMap;
 use std::io::{self, Write};
 
+// command line arguments
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    key: String,
+    #[arg(short, long)]
+    verb: String,
+    #[arg(short, long)]
+    band: String,
+    #[arg(short, long)]
+    album: String,
+    #[arg(short, long)]
+    thoughts: String,
+}
+
 // function to construct the header with the API key
-fn construct_apikey_header() -> HeaderMap {
+fn construct_apikey_header(api_key: String) -> HeaderMap {
     let mut headers = HeaderMap::new();
     headers.insert(
         HeaderName::from_static("x-api-key"),
-        HeaderValue::from_static("metallica_1983"),
+        HeaderValue::from_str(&api_key).expect("Unable to construct header"),
     );
     headers
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // parse the command line arguments
+    let args = Args::parse();
+
     // create a new reqwest client
     let client = reqwest::Client::new();
 
-    // create the url
-    let url = "https://rivet-head-api.shuttleapp.rs/api/diary";
-    
-    // make the request
-    let res = client.get(url).headers(construct_apikey_header()).send().await?;
+    // make the request, depending on the action
+    match args.verb {
+        verb if verb == "get" => {
+            let res = client
+                .get("https://rivet-head-api.shuttleapp.rs/api/diary")
+                .headers(construct_apikey_header(args.key))
+                .send()
+                .await?;
 
-    // print the status code and body
-    let mut stdout = io::stdout();
-    writeln!(stdout, "Status: {}", res.status()).expect("Unable to wite to stdout");
-    let body = res.text().await?;
-    writeln!(stdout, "Body:\n\n{:?}", body).expect("Unable to write to stdout");
+            // print the status code and body
+            let mut stdout = io::stdout();
+            writeln!(stdout, "Status: {}", res.status()).expect("Unable to write to stdout");
+            let body = res.text().await?;
+            writeln!(stdout, "Body:\n\n{:?}", body).expect("Unable to write to stdout");
+        }
+
+        verb if verb == "post" => {
+            let mut params = HashMap::new();
+            params.insert("band_content", args.band);
+            params.insert("album_content", args.album);
+            params.insert("thoughts_content", args.thoughts);
+            let res = client
+                .post("https://rivet-head-api.shuttleapp.rs/api/diary/new")
+                .headers(construct_apikey_header(args.key))
+                .form(&params)
+                .send()
+                .await?;
+
+            // print the status code and body// print the status code and body
+            let mut stdout = io::stdout();
+            writeln!(stdout, "Status: {}", res.status()).expect("Unable to write to stdout");
+            let body = res.text().await?;
+            writeln!(stdout, "Body: {:?}", body).expect("Unable to write to stdout");
+        }
+
+        _ => {
+            let mut stdout = io::stdout();
+            writeln!(stdout, "Invalid verb, enter get, post, put or delete")
+                .expect("Unable to write to stdout");
+        }
+    }
+
     Ok(())
 }
